afb305f6b0eeffba625e5a8acbfa837ad8e3c1ce
br-to <barabara0224@gmail.com>
2025-05-25 00:46:51 +0900

    add: eslint-plugin-react-compilerを追加し、eslint設定を更新

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,6 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat, reactCompiler.configs.recommended];
 
 export default eslintConfig;
