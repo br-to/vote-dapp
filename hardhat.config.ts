637d4b2f38da78d7d8a1d2620dffd1e9b0555113
Toi Kobara <50689373+br-to@users.noreply.github.com>
2025-06-07 18:17:02 +0900

    fix network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,10 +8,10 @@ dotenv.config();
 const config: HardhatUserConfig = {
 	solidity: "0.8.28",
 	networks: {
-		sepolia: {
-			url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-			accounts: [process.env.PRIVATE_KEY || ""],
-		},
+                sepolia: {
+                        url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+                        accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+                },
 	},
 };
 
